20faef65d0112626d27a57b7c420c133691801aa
Raymondkingjnr <raymondnnaji660@gmail.com>
2022-10-13 07:05:06 +0100

    modified:   .gitignore 	modified:   src/Components/Footer.js 	modified:   src/index.css

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,48 +7,55 @@ function Footer() {
   const icons = { color: "grey", marginTop: "0.5em" };
   return (
     <footer>
-      <main className="left--footer">
-        <div>
-          <div className="footer--logo">
-            <FaTree style={style} />
-            <h3>Ray Landscaping</h3>
+      <article className="footer">
+        <main className="left--footer">
+          <div>
+            <div className="footer--logo">
+              <FaTree style={style} />
+              <h3>Ray Landscaping</h3>
+            </div>
+            <p>
+              We're proud of our contribution towards building the
+              future,grounds up! Our landscaping conpany is ready to complete
+              any kind of project for any type of commercial and residential
+              client{" "}
+            </p>
           </div>
-          <p>
-            We're proud of our contribution towards building the future,grounds
-            up! Our landscaping conpany is ready to complete any kind of project
-            for any type of commercial and residential client{" "}
-          </p>
-        </div>
-      </main>
+        </main>
 
-      <main className="right--footer">
-        <div>
-          <div className="footer--contact">
-            <h4>Contact Us</h4>
-            <div className="contact">
-              <FaTelegramPlane style={icons} />
-              <p>Yarmouth MA 02673</p>
-            </div>
-            <div className="contact">
-              <FaMailBulk style={icons} />
-              <p>Contact@ray.com</p>
-            </div>
-            <div className="contact">
-              <BsTelephoneFill style={icons} />
-              <p>(0-01)810-054-899</p>
+        <main className="right--footer">
+          <div>
+            <div className="footer--contact">
+              <h4>Contact Us</h4>
+              <div className="contact">
+                <FaTelegramPlane style={icons} />
+                <p>Yarmouth MA 02673</p>
+              </div>
+              <div className="contact">
+                <FaMailBulk style={icons} />
+                <p>Contact@ray.com</p>
+              </div>
+              <div className="contact">
+                <BsTelephoneFill style={icons} />
+                <p>(0-01)810-054-899</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="media">
-          <h4>Social Media</h4>
-          <p>Facebook</p>
-        </div>
-        <div className="working--hours">
-          <h4>Working hours</h4>
-          <p>Mon-Sat</p>
-          <p>Working Hours &:00 AM - 6:00</p>
-        </div>
-      </main>
+          <div className="media">
+            <h4>Social Media</h4>
+            <p>Facebook</p>
+          </div>
+          <div className="working--hours">
+            <h4>Working hours</h4>
+            <p>Mon-Sat</p>
+            <p>Working Hours &:00 AM - 6:00</p>
+          </div>
+        </main>
+      </article>
+      <p className="p">
+        @ 2022 Ray's Landscaping All rights reserved.Privacy Policy - Cookies
+        Policy
+      </p>
     </footer>
   );
 }
